2e3831f86d1f5f74f3c74ea9ef793688c20278e0
Bastian Raschke <bastian.raschke@posteo.de>
2018-12-27 12:38:06 +0100

    Example project cleanups

--- app/src/main/java/de/sicherheitskritisch/pass/MainActivity.kt
+++ app/src/main/java/de/sicherheitskritisch/pass/MainActivity.kt
@@ -1,85 +1,108 @@
 package de.sicherheitskritisch.pass
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(){
+
+    private val navigationItemSelectedListener = NavigationItemSelectedListener()
+
+    private var toolBar: Toolbar? = null
+    private var fabButton: FloatingActionButton? = null
+    private var drawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        setupToolbar()
+        setupFloatingActionMenu()
+        setupDrawerLayout()
+    }
+
+    private fun setupToolbar() {
+        toolBar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolBar)
+    }
+
+    private fun setupFloatingActionMenu() {
+        fabButton = findViewById<FloatingActionButton>(R.id.main_fab).apply {
+            setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        }
+    }
+
+    private fun setupDrawerLayout() {
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout).apply {
+            val toggle = ActionBarDrawerToggle(
+                this@MainActivity,
+                this,
+                toolBar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+            )
+            addDrawerListener(toggle)
+            toggle.syncState()
         }
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view).apply {
+            setNavigationItemSelectedListener(navigationItemSelectedListener)
+        }
     }
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
+        drawerLayout?.takeIf { it.isDrawerOpen(GravityCompat.START) }?.closeDrawer(GravityCompat.START) ?: run {
             super.onBackPressed()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
+    private inner class NavigationItemSelectedListener : NavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.nav_camera -> {
 
-            }
-            R.id.nav_slideshow -> {
+                }
+                R.id.nav_gallery -> {
 
-            }
-            R.id.nav_manage -> {
+                }
+                R.id.nav_slideshow -> {
 
-            }
-            R.id.nav_share -> {
+                }
+                R.id.nav_manage -> {
 
-            }
-            R.id.nav_send -> {
+                }
+                R.id.nav_share -> {
 
+                }
+                R.id.nav_send -> {
+
+                }
             }
+
+            drawerLayout?.closeDrawer(GravityCompat.START)
+            return true
         }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
