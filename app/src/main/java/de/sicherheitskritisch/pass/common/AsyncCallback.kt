0778d64bd89d449cb66c695fa6d86d5abae3cb90
Bastian Raschke <bastian.raschke@posteo.de>
2019-01-01 17:04:39 +0100

    Added async callback functions

---
+++ app/src/main/java/de/sicherheitskritisch/pass/common/AsyncCallback.kt
@@ -0,0 +1,26 @@
+package de.sicherheitskritisch.pass.common
+
+abstract class AsyncCallback<ResultType, ErrorType> {
+    abstract fun onSuccess(result: ResultType? = null)
+    abstract fun onFailure(error: ErrorType)
+}
+
+sealed class AsyncCallbackResult<ResultType, ErrorType> {
+    class Success<ResultType, ErrorType>(val result: ResultType?) : AsyncCallbackResult<ResultType, ErrorType>()
+    class Failure<ResultType, ErrorType>(val error: ErrorType) : AsyncCallbackResult<ResultType, ErrorType>()
+}
+
+fun <ResultType, ErrorType> asyncCallback(
+    resultHandler: (AsyncCallbackResult<ResultType, ErrorType>) -> Unit
+): AsyncCallback<ResultType, ErrorType> {
+    return object : AsyncCallback<ResultType, ErrorType>() {
+        override fun onSuccess(result: ResultType?) {
+            resultHandler(AsyncCallbackResult.Success(result))
+        }
+
+        override fun onFailure(error: ErrorType) {
+            resultHandler(AsyncCallbackResult.Failure(error))
+        }
+    }
+}
+
