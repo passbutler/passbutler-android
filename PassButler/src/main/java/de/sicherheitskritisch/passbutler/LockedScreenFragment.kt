27fce236fa4ddc4e81ce05f7cddb42623c736a1c
Bastian Raschke <bastian.raschke@posteo.de>
2019-02-11 22:14:31 +0100

    Added locked screen

---
+++ PassButler/src/main/java/de/sicherheitskritisch/passbutler/LockedScreenFragment.kt
@@ -0,0 +1,39 @@
+package de.sicherheitskritisch.passbutler
+
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.sicherheitskritisch.passbutler.databinding.FragmentLockedScreenBinding
+import de.sicherheitskritisch.passbutler.ui.BaseViewModelFragment
+
+class LockedScreenFragment : BaseViewModelFragment<RootViewModel>() {
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        activity?.let {
+            viewModel = ViewModelProviders.of(it).get(RootViewModel::class.java)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentLockedScreenBinding>(inflater, R.layout.fragment_locked_screen, container, false).also { binding ->
+            binding.setLifecycleOwner(this)
+            binding.viewModel = viewModel
+
+            binding.imageViewFingerprintIcon.setOnClickListener {
+                viewModel.unlockScreen()
+            }
+        }
+
+        return binding?.root
+    }
+
+    companion object {
+        fun newInstance() = LockedScreenFragment()
+    }
+}
