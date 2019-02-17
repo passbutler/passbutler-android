f33d4b202a11542e5f6c924cc292fa4b23cb7b25
Bastian Raschke <bastian.raschke@posteo.de>
2019-02-17 21:33:10 +0100

    Added logger workaround

--- PassButler/src/main/java/de/sicherheitskritisch/passbutler/common/Logger.kt
+++ PassButler/src/main/java/de/sicherheitskritisch/passbutler/common/Logger.kt
@@ -9,10 +9,18 @@ object Logger {
     fun d(tag: String, message: String) = Log.d(tag, buildLogString(message))
     fun i(tag: String, message: String) = Log.i(tag, buildLogString(message))
     fun w(tag: String, message: String) = Log.w(tag, buildLogString(message))
-    fun w(tag: String, message: String, throwable: Throwable) = Log.w(tag, buildLogString(message), throwable)
+    fun w(tag: String, message: String, throwable: Throwable) = Log.w(tag, buildLogString(message, throwable))
 }
 
-private fun buildLogString(message: String): String {
+private fun buildLogString(message: String, throwable: Throwable? = null): String {
     val threadName = Thread.currentThread().name
-    return "[$threadName]: $message"
+
+    // The logger swallows `UnknownHostException` exception, so we can't use `Log.x(String, String, Throwable)`
+    val throwableStackTrace = throwable?.let {
+        val throwableMessage = it.message
+        val stacktraceString = it.stackTrace?.joinToString("\n")
+        "\n$throwableMessage\n$stacktraceString"
+    }
+
+    return "[$threadName]: $message$throwableStackTrace"
 }
