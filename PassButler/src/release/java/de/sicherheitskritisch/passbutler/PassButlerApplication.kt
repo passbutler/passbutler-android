7e9788c568259658d679cddb6dbf2d306aaa0cc4
Bastian Raschke <bastian.raschke@posteo.de>
2019-05-13 21:54:16 +0200

    Fixed release build

--- PassButler/src/release/java/de/sicherheitskritisch/passbutler/PassButlerApplication.kt
+++ PassButler/src/release/java/de/sicherheitskritisch/passbutler/PassButlerApplication.kt
@@ -1,6 +1,5 @@
 package de.sicherheitskritisch.passbutler
 
-import android.app.Application
 import de.sicherheitskritisch.passbutler.base.AbstractPassButlerApplication
 
-class PassbutlerApplication : AbstractPassbutlerApplication()
+class PassButlerApplication : AbstractPassButlerApplication()
