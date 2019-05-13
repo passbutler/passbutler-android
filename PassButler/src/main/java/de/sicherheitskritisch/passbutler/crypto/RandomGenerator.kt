5913ba34e97961f8a789d489e119be8e143c42f2
Bastian Raschke <bastian.raschke@posteo.de>
2019-05-13 22:39:21 +0200

    Added method documentation

--- PassButler/src/main/java/de/sicherheitskritisch/passbutler/crypto/RandomGenerator.kt
+++ PassButler/src/main/java/de/sicherheitskritisch/passbutler/crypto/RandomGenerator.kt
@@ -3,8 +3,12 @@ package de.sicherheitskritisch.passbutler.crypto
 import java.security.SecureRandom
 
 object RandomGenerator {
+
+    /**
+     * Generates a desired amount of random bytes. It uses the default `SecureRandom` constructor
+     * that uses `/dev/urandom` that is sufficient secure and does not block.
+     */
     fun generateRandomBytes(count: Int): ByteArray {
-        // Use default `SecureRandom` constructor that uses `/dev/urandom` that is sufficient secure and does not block
         return SecureRandom().let { nonBlockingSecureRandomInstance ->
             val randomBytesArray = ByteArray(count)
             nonBlockingSecureRandomInstance.nextBytes(randomBytesArray)
