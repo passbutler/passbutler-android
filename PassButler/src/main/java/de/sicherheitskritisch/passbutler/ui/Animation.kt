8e1f2f09c7d2b9e85ebc8501f74ad45420ed4b45
Bastian Raschke <bastian.raschke@posteo.de>
2019-09-18 21:18:46 +0200

    Fixed fade in/out animation (set values always on start to be sure also on animation cancel, the values are re-applied)

--- PassButler/src/main/java/de/sicherheitskritisch/passbutler/ui/Animation.kt
+++ PassButler/src/main/java/de/sicherheitskritisch/passbutler/ui/Animation.kt
@@ -7,55 +7,30 @@ import android.view.View
 private const val FADE_ANIMATION_DURATION_MILLISECONDS = 350L
 
 /**
- * Animates view with fade-in only.
- */
-fun View.showFadeInAnimation(shouldShow: Boolean, visibilityHideMode: VisibilityHideMode = VisibilityHideMode.GONE) {
-    val shouldShowVisibilityEquivalent = if (shouldShow) View.VISIBLE else View.GONE
-
-    // Only animate if necessary
-    if (visibility != shouldShowVisibilityEquivalent) {
-        if (shouldShow) {
-            visibility = View.VISIBLE
-
-            animate()
-                .setDuration(FADE_ANIMATION_DURATION_MILLISECONDS)
-                .alpha(1.0f)
-        } else {
-            visibility = visibilityHideMode.viewConstant
-        }
-    }
-}
-
-/**
  * Animates view with fade-in and fade-out.
  */
 fun View.showFadeInOutAnimation(shouldShow: Boolean, visibilityHideMode: VisibilityHideMode = VisibilityHideMode.GONE) {
-    val shouldShowVisibilityEquivalent = if (shouldShow) View.VISIBLE else View.GONE
+    val startAlpha = if (shouldShow) 0.0f else 1.0f
+    val endAlpha = if (shouldShow) 1.0f else 0.0f
 
-    // Only animate if necessary
-    if (visibility != shouldShowVisibilityEquivalent) {
-        val startAlpha = if (shouldShow) 0.0f else 1.0f
-        val endAlpha = if (shouldShow) 1.0f else 0.0f
-
-        // Set initial visibility always to visible, but alpha dependent of state
-        alpha = startAlpha
-        visibility = View.VISIBLE
-
-        animate()
-            .setDuration(FADE_ANIMATION_DURATION_MILLISECONDS)
-            .alpha(endAlpha)
-            .setListener(animatorListenerAdapter(
-                onAnimationStart = {
-                    // Not needed
-                },
-                onAnimationEnd = {
-                    // Only set viability to gone if necessary
-                    if (!shouldShow) {
-                        visibility = visibilityHideMode.viewConstant
-                    }
+    animate()
+        .setDuration(FADE_ANIMATION_DURATION_MILLISECONDS)
+        .alpha(endAlpha)
+        .setListener(animatorListenerAdapter(
+            onAnimationStart = {
+                // If view should be shown, set values always on start to be sure also on animation cancel, the values are re-applied
+                if (shouldShow) {
+                    alpha = startAlpha
+                    visibility = View.VISIBLE
                 }
-            ))
-    }
+            },
+            onAnimationEnd = {
+                // Hide view according to desired hide mode if necessary
+                if (!shouldShow) {
+                    visibility = visibilityHideMode.viewConstant
+                }
+            }
+        ))
 }
 
 enum class VisibilityHideMode {
