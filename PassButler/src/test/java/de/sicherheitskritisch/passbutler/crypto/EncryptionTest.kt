b482419736ee2561f4f0ead4854556e41490953c
Bastian Raschke <bastian.raschke@posteo.de>
2019-10-20 21:16:32 +0200

    Concretized naming of encryption tests

--- PassButler/src/test/java/de/sicherheitskritisch/passbutler/crypto/EncryptionTest.kt
+++ PassButler/src/test/java/de/sicherheitskritisch/passbutler/crypto/EncryptionTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
-class EncryptionTest {
+class SymmetricEncryptionTest {
 
     /**
      * AES-256-GCM encryption tests
@@ -203,7 +203,7 @@ class EncryptionTest {
     companion object {
 
         private val invalidTestVectors = mapOf(
-            "emptyInitializationVector" to TestVector(
+            "emptyInitializationVector" to SymmetricTestVector(
                 key = "0000000000000000000000000000000000000000000000000000000000000000",
                 initializationVector = "",
                 plainText = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -211,7 +211,7 @@ class EncryptionTest {
                 tag = "00000000000000000000000000000000"
             ),
 
-            "tooLongInitializationVector" to TestVector(
+            "tooLongInitializationVector" to SymmetricTestVector(
                 key = "0000000000000000000000000000000000000000000000000000000000000000",
                 initializationVector = "000000000000000000000000AA",
                 plainText = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -219,7 +219,7 @@ class EncryptionTest {
                 tag = "00000000000000000000000000000000"
             ),
 
-            "emptyKey" to TestVector(
+            "emptyKey" to SymmetricTestVector(
                 key = "",
                 initializationVector = "000000000000000000000000",
                 plainText = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -227,7 +227,7 @@ class EncryptionTest {
                 tag = "00000000000000000000000000000000"
             ),
 
-            "tooLongKey" to TestVector(
+            "tooLongKey" to SymmetricTestVector(
                 key = "0000000000000000000000000000000000000000000000000000000000000000AA",
                 initializationVector = "000000000000000000000000",
                 plainText = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -250,7 +250,7 @@ class EncryptionTest {
              *
              * (from file `gcmDecrypt256.rsp`)
             */
-            TestVector(
+            SymmetricTestVector(
                 key = "F5A2B27C74355872EB3EF6C5FEAFAA740E6AE990D9D48C3BD9BB8235E589F010",
                 initializationVector = "58D2240F580A31C1D24948E9",
                 plainText = "",
@@ -268,7 +268,7 @@ class EncryptionTest {
              * (from file `gcmEncryptExtIV256.rsp`)
              */
 
-            TestVector(
+            SymmetricTestVector(
                 key = "1FDED32D5999DE4A76E0F8082108823AEF60417E1896CF4218A2FA90F632EC8A",
                 initializationVector = "1F3AFA4711E9474F32E70462",
                 plainText = "06B2C75853DF9AEB17BEFD33CEA81C630B0FC53667FF45199C629C8E15DCE41E530AA792F796B8138EEAB2E86C7B7BEE1D40B0",
@@ -276,7 +276,7 @@ class EncryptionTest {
                 tag = "30096D340F3D5C42D82A6F475DEF23EB"
             ),
 
-            TestVector(
+            SymmetricTestVector(
                 key = "B405AC89724F8B555BFEE1EAA369CD854003E9FAE415F28C5A199D4D6EFC83D6",
                 initializationVector = "CEC71A13B14C4D9BD024EF29",
                 plainText = "AB4FD35BEF66ADDFD2856B3881FF2C74FDC09C82ABE339F49736D69B2BD0A71A6B4FE8FC53F50F8B7D6D6D6138AB442C7F653F",
@@ -284,7 +284,7 @@ class EncryptionTest {
                 tag = "6D2B7861A3C59BA5A3E3A11C92BB2B14"
             ),
 
-            TestVector(
+            SymmetricTestVector(
                 key = "FAD40C82264DC9B8D9A42C10A234138344B0133A708D8899DA934BFEE2BDD6B8",
                 initializationVector = "0DADE2C95A9B85A8D2BC13EF",
                 plainText = "664EA95D511B2CFDB9E5FB87EFDD41CBFB88F3FF47A7D2B8830967E39071A89B948754FFB0ED34C357ED6D4B4B2F8A76615C03",
@@ -292,7 +292,7 @@ class EncryptionTest {
                 tag = "5CA8905D469FFFEC6FBA7435EBDFFDAF"
             ),
 
-            TestVector(
+            SymmetricTestVector(
                 key = "AA5FCA688CC83283ECF39454679948F4D30AA8CB43DB7CC4DA4EFF1669D6C52F",
                 initializationVector = "4B2D7B699A5259F9B541FA49",
                 plainText = "C691F3B8F3917EFB76825108C0E37DC33E7A8342764CE68A62A2DC1A5C940594961FCD5C0DF05394A5C0FFF66C254C6B26A549",
@@ -300,7 +300,7 @@ class EncryptionTest {
                 tag = "5DE4EF67BF8896FBE82C01DCA041D590"
             ),
 
-            TestVector(
+            SymmetricTestVector(
                 key = "1C7690D5D845FCEABBA227B11CA221F4D6D302233641016D9CD3A158C3E36017",
                 initializationVector = "93BCA8DE6B11A4830C5F5F64",
                 plainText = "3C79A39878A605F3AC63A256F68C8A66369CC3CD7AF680D19692B485A7BA58CE1D536707C55EDA5B256C8B29BBF0B4CBEB4FC4",
@@ -309,17 +309,17 @@ class EncryptionTest {
             )
         )
 
-        private fun encryptAES256GCM(testVector: TestVector): String {
+        private fun encryptAES256GCM(testVector: SymmetricTestVector): String {
             return EncryptionAlgorithm.Symmetric.AES256GCM.encrypt(testVector.initializationVector.hexToBytes(), testVector.key.hexToBytes(), testVector.plainText.hexToBytes()).toHexString()
         }
 
-        private fun decryptAES256GCM(testVector: TestVector): String {
+        private fun decryptAES256GCM(testVector: SymmetricTestVector): String {
             return EncryptionAlgorithm.Symmetric.AES256GCM.decrypt(testVector.initializationVector.hexToBytes(), testVector.key.hexToBytes(), (testVector.cipherText + testVector.tag).hexToBytes()).toHexString()
         }
     }
 }
 
-private data class TestVector(
+private data class SymmetricTestVector(
     val key: String,
     val initializationVector: String,
     val plainText: String,
