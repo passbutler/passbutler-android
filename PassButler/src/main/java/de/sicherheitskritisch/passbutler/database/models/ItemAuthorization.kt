6a845f88502491d48b8f19d7c52075540cfb4dd4
Bastian Raschke <bastian.raschke@posteo.de>
2019-12-27 15:42:49 +0100

    Implemented tests for `ItemAuthorization`

--- PassButler/src/main/java/de/sicherheitskritisch/passbutler/database/models/ItemAuthorization.kt
+++ PassButler/src/main/java/de/sicherheitskritisch/passbutler/database/models/ItemAuthorization.kt
@@ -6,13 +6,19 @@ import androidx.room.ForeignKey
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import de.sicherheitskritisch.passbutler.base.JSONSerializable
+import de.sicherheitskritisch.passbutler.base.JSONSerializableDeserializer
+import de.sicherheitskritisch.passbutler.base.getDate
+import de.sicherheitskritisch.passbutler.base.putBoolean
+import de.sicherheitskritisch.passbutler.base.putDate
+import de.sicherheitskritisch.passbutler.base.putString
 import de.sicherheitskritisch.passbutler.crypto.models.CryptographicKey
 import de.sicherheitskritisch.passbutler.crypto.models.ProtectedValue
+import de.sicherheitskritisch.passbutler.crypto.models.getProtectedValue
+import de.sicherheitskritisch.passbutler.crypto.models.putProtectedValue
 import de.sicherheitskritisch.passbutler.database.Synchronizable
+import org.json.JSONException
 import org.json.JSONObject
 import java.util.*
-
-// TODO: Add tests
 
 @Entity(
     tableName = "item_authorizations",
@@ -49,7 +55,42 @@ data class ItemAuthorization(
     override val primaryField = id
 
     override fun serialize(): JSONObject {
-        // TODO: Implement
-        return JSONObject()
+        return JSONObject().apply {
+            putString(SERIALIZATION_KEY_ID, id)
+            putString(SERIALIZATION_KEY_USER_ID, userId)
+            putString(SERIALIZATION_KEY_ITEM_ID, itemId)
+            putProtectedValue(SERIALIZATION_KEY_ITEM_KEY, itemKey)
+            putBoolean(SERIALIZATION_KEY_READONLY, readOnly)
+            putBoolean(SERIALIZATION_KEY_DELETED, deleted)
+            putDate(SERIALIZATION_KEY_MODIFIED, modified)
+            putDate(SERIALIZATION_KEY_CREATED, created)
+        }
+    }
+
+    object Deserializer : JSONSerializableDeserializer<ItemAuthorization>() {
+        @Throws(JSONException::class)
+        override fun deserialize(jsonObject: JSONObject): ItemAuthorization {
+            return ItemAuthorization(
+                id = jsonObject.getString(SERIALIZATION_KEY_ID),
+                userId = jsonObject.getString(SERIALIZATION_KEY_USER_ID),
+                itemId = jsonObject.getString(SERIALIZATION_KEY_ITEM_ID),
+                itemKey = jsonObject.getProtectedValue(SERIALIZATION_KEY_ITEM_KEY),
+                readOnly = jsonObject.getBoolean(SERIALIZATION_KEY_READONLY),
+                deleted = jsonObject.getBoolean(SERIALIZATION_KEY_DELETED),
+                modified = jsonObject.getDate(SERIALIZATION_KEY_MODIFIED),
+                created = jsonObject.getDate(SERIALIZATION_KEY_CREATED)
+            )
+        }
+    }
+
+    companion object {
+        private const val SERIALIZATION_KEY_ID = "id"
+        private const val SERIALIZATION_KEY_USER_ID = "userId"
+        private const val SERIALIZATION_KEY_ITEM_ID = "itemId"
+        private const val SERIALIZATION_KEY_ITEM_KEY = "itemKey"
+        private const val SERIALIZATION_KEY_READONLY = "readOnly"
+        private const val SERIALIZATION_KEY_DELETED = "deleted"
+        private const val SERIALIZATION_KEY_MODIFIED = "modified"
+        private const val SERIALIZATION_KEY_CREATED = "created"
     }
 }
---
+++ PassButler/src/test/java/de/sicherheitskritisch/passbutler/database/models/ItemAuthorizationTest.kt
@@ -0,0 +1,114 @@
+package de.sicherheitskritisch.passbutler.database.models
+
+import android.util.Log
+import de.sicherheitskritisch.passbutler.assertJSONObjectEquals
+import de.sicherheitskritisch.passbutler.crypto.EncryptionAlgorithm
+import de.sicherheitskritisch.passbutler.crypto.models.CryptographicKey
+import de.sicherheitskritisch.passbutler.crypto.models.ProtectedValue
+import de.sicherheitskritisch.passbutler.hexToBytes
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import org.json.JSONObject
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ItemAuthorizationTest {
+
+    @BeforeEach
+    fun setUp() {
+        mockkStatic(Log::class)
+        every { Log.d(any(), any()) } returns 0
+    }
+
+    @AfterEach
+    fun unsetUp() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `Serialize and deserialize a ItemAuthorization should result an equal object`() {
+        val exampleItemAuthorization = createExampleItemAuthorization()
+
+        val serializedItemAuthorization = exampleItemAuthorization.serialize()
+        val deserializedItemAuthorization = ItemAuthorization.Deserializer.deserializeOrNull(serializedItemAuthorization)
+
+        assertEquals(exampleItemAuthorization, deserializedItemAuthorization)
+    }
+
+    @Test
+    fun `Serialize an ItemAuthorization`() {
+        val exampleItemAuthorization = createExampleItemAuthorization()
+        val expectedSerialized = createSerializedExampleItemAuthorization()
+
+        assertJSONObjectEquals(expectedSerialized, exampleItemAuthorization.serialize())
+    }
+
+    @Test
+    fun `Deserialize an ItemAuthorization`() {
+        val serializedItemAuthorization = createSerializedExampleItemAuthorization()
+        val expectedItemAuthorization = createExampleItemAuthorization()
+
+        assertEquals(expectedItemAuthorization, ItemAuthorization.Deserializer.deserializeOrNull(serializedItemAuthorization))
+    }
+
+    @Test
+    fun `Deserialize an invalid ItemAuthorization returns null`() {
+        val serializedItemAuthorization = JSONObject(
+            """{"foo":"bar"}"""
+        )
+        val expectedItemAuthorization = null
+
+        assertEquals(expectedItemAuthorization, ItemAuthorization.Deserializer.deserializeOrNull(serializedItemAuthorization))
+    }
+
+    companion object {
+        private fun createExampleItemAuthorization(): ItemAuthorization {
+            val itemKey = ProtectedValue.createInstanceForTesting<CryptographicKey>(
+                "AAAAAAAAAAAAAAAAAAAAAAAA".hexToBytes(),
+                "0000000000000000000000000000000000000000000000000000000000000000".hexToBytes(),
+                EncryptionAlgorithm.Symmetric.AES256GCM
+            )
+
+            return ItemAuthorization(
+                id = "exampleId",
+                userId = "exampleUserId",
+                itemId = "exampleItemId",
+                itemKey = itemKey,
+                readOnly = true,
+                deleted = true,
+                modified = "2019-12-27T12:00:01+0000".toDate(),
+                created = "2019-12-27T12:00:00+0000".toDate()
+            )
+        }
+
+        private fun createSerializedExampleItemAuthorization(): JSONObject {
+            return JSONObject(
+                """
+                {
+                  "id": "exampleId",
+                  "itemId": "exampleItemId",
+                  "userId": "exampleUserId",
+                  "itemKey": {
+                    "encryptedValue": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                    "encryptionAlgorithm": "AES-256-GCM",
+                    "initializationVector": "qqqqqqqqqqqqqqqq"
+                  },
+                  "readOnly": true,
+                  "deleted": true,
+                  "modified": 1577448001000,
+                  "created": 1577448000000,
+                }
+                """.trimIndent()
+            )
+        }
+
+        private fun String.toDate(): Date {
+            return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(this)!!
+        }
+    }
+}
