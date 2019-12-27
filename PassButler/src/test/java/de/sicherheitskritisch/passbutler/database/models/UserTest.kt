0939420a9c3dab3b4bfc8d3e2f98fa291029c7f8
Bastian Raschke <bastian.raschke@posteo.de>
2019-12-27 16:19:52 +0100

    Extended tests for `User`

--- PassButler/src/test/java/de/sicherheitskritisch/passbutler/database/models/UserTest.kt
+++ PassButler/src/test/java/de/sicherheitskritisch/passbutler/database/models/UserTest.kt
@@ -1,36 +1,21 @@
 package de.sicherheitskritisch.passbutler.database.models
 
+import de.sicherheitskritisch.passbutler.assertJSONObjectEquals
 import de.sicherheitskritisch.passbutler.crypto.EncryptionAlgorithm
 import de.sicherheitskritisch.passbutler.crypto.models.CryptographicKey
 import de.sicherheitskritisch.passbutler.crypto.models.KeyDerivationInformation
 import de.sicherheitskritisch.passbutler.crypto.models.ProtectedValue
 import de.sicherheitskritisch.passbutler.hexToBytes
-import io.mockk.mockkObject
+import de.sicherheitskritisch.passbutler.toDate
+import org.json.JSONObject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.*
 
 class UserTest {
 
     @Test
     fun `Serialize and deserialize a User should result an equal object`() {
-        mockkObject(ProtectedValue.Companion)
-
-        val modifiedDate = 12345678L
-        val createdDate = 12345679L
-
-        val exampleUser = User(
-            "myUserName",
-            createTestMasterPasswordAuthenticationHash(),
-            createTestKeyDerivationInformation(),
-            createTestProtectedValueMasterEncryptionKey(),
-            createTestItemEncryptionPublicKey(),
-            createTestItemEncryptionSecretKey(),
-            createTestProtectedValueSettings(),
-            true,
-            Date(modifiedDate),
-            Date(createdDate)
-        )
+        val exampleUser = createExampleUser()
 
         val serializedUser = exampleUser.serialize()
         val deserializedUser = User.DefaultUserDeserializer.deserializeOrNull(serializedUser)
@@ -38,7 +23,85 @@ class UserTest {
         assertEquals(exampleUser, deserializedUser)
     }
 
+    @Test
+    fun `Serialize an User`() {
+        val exampleUser = createExampleUser()
+        val expectedSerialized = createSerializedExampleUser()
+
+        assertJSONObjectEquals(expectedSerialized, exampleUser.serialize())
+    }
+
+    @Test
+    fun `Deserialize an User`() {
+        val serializedUser = createSerializedExampleUser()
+        val expectedUser = createExampleUser()
+
+        assertEquals(expectedUser, User.DefaultUserDeserializer.deserializeOrNull(serializedUser))
+    }
+
+    @Test
+    fun `Deserialize an invalid User returns null`() {
+        val serializedUser = JSONObject(
+            """{"foo":"bar"}"""
+        )
+        val expectedUser = null
+
+        assertEquals(expectedUser, User.DefaultUserDeserializer.deserializeOrNull(serializedUser))
+    }
+
     companion object {
+        private fun createExampleUser(): User {
+
+            return User(
+                username = "myUserName",
+                masterPasswordAuthenticationHash = createTestMasterPasswordAuthenticationHash(),
+                masterKeyDerivationInformation = createTestKeyDerivationInformation(),
+                masterEncryptionKey = createTestProtectedValueMasterEncryptionKey(),
+                itemEncryptionPublicKey = createTestItemEncryptionPublicKey(),
+                itemEncryptionSecretKey = createTestItemEncryptionSecretKey(),
+                settings = createTestProtectedValueSettings(),
+                deleted = true,
+                modified = "2019-12-27T12:00:01+0000".toDate(),
+                created = "2019-12-27T12:00:00+0000".toDate()
+            )
+        }
+
+        private fun createSerializedExampleUser(): JSONObject {
+            return JSONObject(
+                """
+                {
+                  "username": "myUserName",
+                  "masterPasswordAuthenticationHash": "pbkdf2:sha256:150000${'$'}nww6C11M${'$'}241ac264e71f35826b8a475bdeb8c6b231a4de2b228f7af979f246c24b4905de",
+                  "masterKeyDerivationInformation": {
+                    "iterationCount": 1234,
+                    "salt": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+                  },
+                  "masterEncryptionKey": {
+                    "initializationVector": "qqqqqqqqqqqqqqqq",
+                    "encryptedValue": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                    "encryptionAlgorithm": "AES-256-GCM"
+                  },
+                  "itemEncryptionPublicKey": {
+                    "key": "qrvM"
+                  },
+                  "itemEncryptionSecretKey": {
+                    "initializationVector": "zMzMzMzMzMzMzMzM",
+                    "encryptedValue": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                    "encryptionAlgorithm": "AES-256-GCM"
+                  },
+                  "settings": {
+                    "initializationVector": "u7u7u7u7u7u7u7u7",
+                    "encryptedValue": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                    "encryptionAlgorithm": "AES-256-GCM"
+                  },
+                  "deleted": true,
+                  "modified": 1577448001000,
+                  "created": 1577448000000
+                }
+                """.trimIndent()
+            )
+        }
+
         private fun createTestMasterPasswordAuthenticationHash(): String {
             return "pbkdf2:sha256:150000\$nww6C11M\$241ac264e71f35826b8a475bdeb8c6b231a4de2b228f7af979f246c24b4905de"
         }
