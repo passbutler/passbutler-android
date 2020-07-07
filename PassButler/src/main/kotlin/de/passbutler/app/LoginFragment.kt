de59b472e97be399e1e9caa7a860bbc92734735d
Bastian Raschke <bastian.raschke@posteo.de>
2020-07-07 22:38:44 +0200

    Final cleanup

--- PassButler/src/main/kotlin/de/passbutler/app/LoginFragment.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/LoginFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.URLUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import de.passbutler.app.base.BuildInformationProvider
 import de.passbutler.app.base.DebugConstants
 import de.passbutler.app.base.launchRequestSending
--- PassButler/src/main/kotlin/de/passbutler/app/RegisterLocalUserFragment.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/RegisterLocalUserFragment.kt
@@ -64,7 +64,8 @@ class RegisterLocalUserFragment : ToolBarFragment() {
                     binding.textInputLayoutServerurl, binding.textInputEditTextServerurl, listOfNotNull(
                         FormFieldValidator.Rule({ TextUtils.isEmpty(it) }, getString(R.string.form_serverurl_validation_error_empty)),
                         FormFieldValidator.Rule({ !URLUtil.isValidUrl(it) }, getString(R.string.form_serverurl_validation_error_invalid)),
-                        FormFieldValidator.Rule({ !URLUtil.isHttpsUrl(it) }, getString(R.string.form_serverurl_validation_error_invalid_scheme)).takeIf { BuildInformationProvider.buildType == BuildType.Release }
+                        FormFieldValidator.Rule({ !URLUtil.isHttpsUrl(it) }, getString(R.string.form_serverurl_validation_error_invalid_scheme))
+                            .takeIf { BuildInformationProvider.buildType == BuildType.Release }
                     )
                 ),
                 FormFieldValidator(
