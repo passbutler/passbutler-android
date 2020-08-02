0c048473065133601354653ab68dd3023034647c
Bastian Raschke <bastian.raschke@posteo.de>
2020-08-02 17:08:00 +0200

    Integrated registration invitation code feature

--- PassButler/src/debug/kotlin/de/passbutler/app/base/DebugConstants.kt
+++ PassButler/src/debug/kotlin/de/passbutler/app/base/DebugConstants.kt
@@ -2,6 +2,7 @@ package de.passbutler.app.base
 
 object DebugConstants {
     const val TEST_SERVERURL = "http://10.0.0.20:5000"
+    const val TEST_INVITATION_CODE = "AAAA-BBBB-CCCC-DDDD"
     const val TEST_USERNAME = "bastian@passbutler.de"
     const val TEST_PASSWORD = "1234"
 }
--- PassButler/src/release/kotlin/de/passbutler/app/base/DebugConstants.kt
+++ PassButler/src/release/kotlin/de/passbutler/app/base/DebugConstants.kt
@@ -2,6 +2,7 @@ package de.passbutler.app.base
 
 object DebugConstants {
     const val TEST_SERVERURL = ""
+    const val TEST_INVITATION_CODE = ""
     const val TEST_USERNAME = ""
     const val TEST_PASSWORD = ""
 }
