c2cca0e0bec6c5973306068356c5e8b27e7fd3be
Bastian Raschke <bastian.raschke@posteo.de>
2020-10-31 16:17:56 +0100

    Moved `showFadeInOutAnimation` to `ViewExtensions` file

--- PassButler/src/main/kotlin/de/passbutler/app/ui/ViewExtensions.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/ui/ViewExtensions.kt
@@ -1,5 +1,7 @@
 package de.passbutler.app.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
@@ -7,6 +9,7 @@ import android.graphics.drawable.Drawable
 import android.util.TypedValue
 import android.view.View
 import androidx.annotation.AttrRes
+import de.passbutler.common.ui.FADE_TRANSITION_DURATION
 
 var View.visible: Boolean
     get() = visibility == View.VISIBLE
@@ -39,4 +42,59 @@ fun Context.resolveThemeAttributeId(@AttrRes attribute: Int): Int {
  */
 fun Drawable.applyTint(color: Int) {
     colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN)
+}
+
+/**
+ * Animates view with fade-in and fade-out.
+ */
+fun View.showFadeInOutAnimation(shouldShow: Boolean, visibilityHideMode: VisibilityHideMode = VisibilityHideMode.GONE) {
+    val startAlpha = if (shouldShow) 0.0f else 1.0f
+    val endAlpha = if (shouldShow) 1.0f else 0.0f
+
+    val viewPropertyAnimator = animate()
+
+    viewPropertyAnimator
+        .setDuration(FADE_TRANSITION_DURATION.toMillis())
+        .alpha(endAlpha)
+        .setListener(animatorListenerAdapter(
+            onAnimationStart = {
+                // If view should be shown, set values always on start to be sure also on animation cancel, the values are re-applied
+                if (shouldShow) {
+                    alpha = startAlpha
+                    visibility = View.VISIBLE
+                }
+            },
+            onAnimationEnd = {
+                // Hide view according to desired hide mode if necessary
+                if (!shouldShow) {
+                    visibility = visibilityHideMode.viewConstant
+                }
+
+                // Remove this listener to avoid it is called if view is animated from other location (the `ViewPropertyAnimator` stays the same)
+                viewPropertyAnimator.setListener(null)
+            }
+        ))
+}
+
+enum class VisibilityHideMode {
+    INVISIBLE,
+    GONE;
+
+    val viewConstant: Int
+        get() = when (this) {
+            INVISIBLE -> View.INVISIBLE
+            GONE -> View.GONE
+        }
+}
+
+inline fun animatorListenerAdapter(crossinline onAnimationEnd: () -> Unit, crossinline onAnimationStart: () -> Unit): AnimatorListenerAdapter {
+    return object : AnimatorListenerAdapter() {
+        override fun onAnimationStart(animation: Animator) {
+            onAnimationStart.invoke()
+        }
+
+        override fun onAnimationEnd(animation: Animator) {
+            onAnimationEnd.invoke()
+        }
+    }
 }
