2a3e3a97d9764d61c89028a73be7235bf8d194af
Bastian Raschke <bastian.raschke@posteo.de>
2020-11-14 14:09:42 +0100

    Added TODO

--- PassButler/src/main/kotlin/de/passbutler/app/ui/UIPresenter.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/ui/UIPresenter.kt
@@ -23,6 +23,7 @@ import java.time.Instant
  * Provides implementation of fragment management used by the one-activity app concept.
  * This delegate is delegated in `BaseFragment` and provided in the `AbstractRootFragment`.
  */
+// TODO: Weak refs should not be needed
 class UIPresenter(
     private val activityWeakReference: WeakReference<Activity>,
     private val rootFragmentWeakReference: WeakReference<AbstractRootFragment>,
