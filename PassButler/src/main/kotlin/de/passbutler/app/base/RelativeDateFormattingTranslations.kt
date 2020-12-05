3c44955294365ed790c9266a199875f6ab07a58e
Bastian Raschke <bastian.raschke@posteo.de>
2020-12-05 10:39:04 +0100

    Renamed string keys for relative date formatting to reflect purpose; Corrected grammatical mistake in German

--- PassButler/src/main/kotlin/de/passbutler/app/base/RelativeDateFormattingTranslations.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/base/RelativeDateFormattingTranslations.kt
@@ -9,13 +9,13 @@ import de.passbutler.common.base.UnitTranslations
 fun createRelativeDateFormattingTranslations(context: Context): RelativeDateFormattingTranslations {
     return RelativeDateFormattingTranslations(
         unitTranslations = UnitTranslations(
-            second = UnitTranslation(context.getString(R.string.general_unit_second_one), context.getString(R.string.general_unit_second_other)),
-            minute = UnitTranslation(context.getString(R.string.general_unit_minute_one), context.getString(R.string.general_unit_minute_other)),
-            hour = UnitTranslation(context.getString(R.string.general_unit_hour_one), context.getString(R.string.general_unit_hour_other)),
-            day = UnitTranslation(context.getString(R.string.general_unit_day_one), context.getString(R.string.general_unit_day_other)),
-            month = UnitTranslation(context.getString(R.string.general_unit_month_one), context.getString(R.string.general_unit_month_other)),
-            year = UnitTranslation(context.getString(R.string.general_unit_year_one), context.getString(R.string.general_unit_year_other))
+            second = UnitTranslation(context.getString(R.string.general_relative_date_unit_second_one), context.getString(R.string.general_relative_date_unit_second_other)),
+            minute = UnitTranslation(context.getString(R.string.general_relative_date_unit_minute_one), context.getString(R.string.general_relative_date_unit_minute_other)),
+            hour = UnitTranslation(context.getString(R.string.general_relative_date_unit_hour_one), context.getString(R.string.general_relative_date_unit_hour_other)),
+            day = UnitTranslation(context.getString(R.string.general_relative_date_unit_day_one), context.getString(R.string.general_relative_date_unit_day_other)),
+            month = UnitTranslation(context.getString(R.string.general_relative_date_unit_month_one), context.getString(R.string.general_relative_date_unit_month_other)),
+            year = UnitTranslation(context.getString(R.string.general_relative_date_unit_year_one), context.getString(R.string.general_relative_date_unit_year_other))
         ),
-        sinceString = context.getString(R.string.general_since)
+        sinceString = context.getString(R.string.general_relative_date_since)
     )
 }
