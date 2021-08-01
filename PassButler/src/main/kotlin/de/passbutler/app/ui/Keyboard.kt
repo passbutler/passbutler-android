6bf1c998324bd569d26e4eae7209fd3acc74b1ae
Bastian Raschke <bastian.raschke@posteo.de>
2021-08-01 12:56:52 +0200

    Fixed issue the user needs to click backpress twice if he wanted to go back from screen from B to A when A hided the keyboard programmatically because the focus was still not cleared

--- PassButler/src/main/kotlin/de/passbutler/app/ui/Keyboard.kt
+++ PassButler/src/main/kotlin/de/passbutler/app/ui/Keyboard.kt
@@ -5,14 +5,20 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import org.tinylog.kotlin.Logger
 
 object Keyboard {
     fun hideKeyboard(context: Context?, view: View?) {
-        val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager
+        val inputMethodManager = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager
+        val viewWindowToken = view?.windowToken
 
-        view?.windowToken?.let { viewWindowToken ->
+        if (inputMethodManager != null && viewWindowToken != null) {
             val defaultFlag = InputMethodManager.RESULT_UNCHANGED_SHOWN
-            imm?.hideSoftInputFromWindow(viewWindowToken, defaultFlag)
+            inputMethodManager.hideSoftInputFromWindow(viewWindowToken, defaultFlag)
+
+            view.clearFocus()
+        } else {
+            Logger.warn("The input method manager or the window token of the view is null!")
         }
     }
 
